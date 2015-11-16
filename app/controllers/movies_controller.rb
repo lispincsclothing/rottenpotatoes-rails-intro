8046432eb1cb69ef175deb0eba4b7b021938cf60
lispincsclothing <scullingnull+1@gmail.com>
2015-11-16 15:43:32 -0800

    hw 2 part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,16 +11,40 @@ class MoviesController < ApplicationController
   end
 
   def index
+    if (params[:sort_by].blank? && params[:ratings].blank? && !session[:sort_by].blank?)
+      flash.keep
+      redirect_to :sort_by => session[:sort_by], :ratings => session[:ratings]
+    end
+
+    if params[:sort_by]
+      @sort_by = params[:sort_by]
+    elsif session[:sort_by]
+      @sort_by = session[:sort_by]
+    else
+      @sort_by = 'id'
+    end
+    if params[:ratings]
+      @ratings_hash = params[:ratings]
+    elsif session[:ratings]
+      @ratings_hash = session[:ratings]
+    else
+      @ratings_hash = Hash[Movie.all_ratings.map {|v| [v,v]}]
+    end
+
     @ratings_hash = params[:ratings] ? params[:ratings] : Hash[Movie.all_ratings.map {|v| [v,v]}]
+    @all_ratings=Movie.all_ratings
+
+    session[:sort_by] = @sort_by
+    session[:ratings] = @ratings_hash
+
     @movies = params[:ratings] ? Movie.where("rating IN (?)", @ratings_hash.keys) : Movie.all
-    @movies.order!(params[:sort_by]) if params[:sort_by]
-    case params[:sort_by]
+    @movies.order!(@sort_by)
+    case @sort_by
     when 'title'
       @title_header='hilite'
     when 'release_date'
       @release_header='hilite'
     end
-    @all_ratings=Movie.all_ratings
   end
 
   def new
